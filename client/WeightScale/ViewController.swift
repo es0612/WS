ace39b339a85d7d35637cc34df76e9410099a599
hyai <hyai0323@gmail.com>
2019-04-18 01:06:45 +0900

    create xcode project.

---
+++ client/WeightScale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeightScale
+//
+//  Created by Ema Shinya on 2019/04/18.
+//  Copyright © 2019年 Ema Shinya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
