d11c71c86d3f3a0372365ace0d353bb58bf11533
hyai <hyai0323@gmail.com>
2019-04-19 19:45:37 +0900

    refactor test and implement on InputVC.

--- client/WeightScaleTests/InputViewControllerSpec.swift
+++ client/WeightScaleTests/InputViewControllerSpec.swift
@@ -7,25 +7,28 @@ import Succinct
 class InputViewControllerSpec: QuickSpec {
     override func spec() {
         describe("Input view controller に関するテスト") {
+            var inputViewController: InputViewController!
+
+            beforeEach {
+                inputViewController = InputViewController()
+            }
+
             it("OKボタンが見える") {
-                let inputViewController = InputViewController()
-
-
-                expect(inputViewController.hasButton(withExactText: "OK")).to(beTrue())
+                expect(inputViewController
+                    .hasButton(withExactText: "OK"))
+                    .to(beTrue())
             }
 
             it("入力欄が見える") {
-                let inputViewController = InputViewController()
-
-
-                expect(inputViewController.hasTextField(withExactPlaceholderText: "00.0")).to(beTrue())
+                expect(inputViewController
+                    .hasTextField(withExactPlaceholderText: "00.0"))
+                    .to(beTrue())
             }
 
             it("kgのラベルが見える") {
-                let inputViewController = InputViewController()
-
-
-                expect(inputViewController.hasLabel(withExactText: "kg")).to(beTrue())
+                expect(inputViewController
+                    .hasLabel(withExactText: "kg"))
+                    .to(beTrue())
             }
 
         }
