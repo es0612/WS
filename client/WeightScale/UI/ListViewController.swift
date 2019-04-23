757a943b1763554a1746ed06f5c94f1b4b995b1f
hyai <hyai0323@gmail.com>
2019-04-23 23:08:40 +0900

    add title for List VC

--- client/WeightScale/UI/ListViewController.swift
+++ client/WeightScale/UI/ListViewController.swift
@@ -1,5 +1,15 @@
 import  UIKit
 
 class ListViewController: UIViewController {
+    init() {
 
+        super.init(nibName: nil, bundle: nil)
+        self.title = "リスト"
+        self.view.backgroundColor = .white
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
