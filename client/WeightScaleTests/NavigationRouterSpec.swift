39d9af1a59f8c544a82b6093b9c66a70a3f6c892
hyai <hyai0323@gmail.com>
2019-04-25 07:42:20 +0900

    テストのリファクタと再表示テスト

--- client/WeightScaleTests/NavigationRouterSpec.swift
+++ client/WeightScaleTests/NavigationRouterSpec.swift
@@ -19,7 +19,10 @@ class NavigationRouterSpec: QuickSpec {
             }
 
             describe("tab barについてのテスト") {
-                it("List画面を初期表示する") {
+
+                var tabBarController: UITabBarController!
+
+                beforeEach {
                     let router = NavigationRouter(animated: false)
                     router.rootViewController = UIViewController()
 
@@ -30,45 +33,36 @@ class NavigationRouterSpec: QuickSpec {
 
                     router.showMainTabBarScreen()
 
-                    
-                    let tabBarController = router.rootViewController?.presentedViewController as! UITabBarController
+
+                    tabBarController =
+                        router.rootViewController?.presentedViewController
+                        as? UITabBarController
+                }
+
+                it("リスト画面を初期表示する") {
                     expect(tabBarController.displayedTab(isAKindOf: ListViewController.self)).to(beTrue())
                 }
 
                 it("設定画面を表示する") {
-                    let router = NavigationRouter(animated: false)
-                    router.rootViewController = UIViewController()
-
-                    let window = UIWindow(frame: UIScreen.main.bounds)
-                    window.makeKeyAndVisible()
-                    window.rootViewController = router.rootViewController
-
-
-                    router.showMainTabBarScreen()
-
-
-                    let tabBarController = router.rootViewController?.presentedViewController as! UITabBarController
-
                     tabBarController.selectTab(withTitle: "設定")
+
+
                     expect(tabBarController.displayedTab(isAKindOf: SettingViewController.self)).to(beTrue())
                 }
 
                 it("グラフ画面を表示する") {
-                    let router = NavigationRouter(animated: false)
-                    router.rootViewController = UIViewController()
-
-                    let window = UIWindow(frame: UIScreen.main.bounds)
-                    window.makeKeyAndVisible()
-                    window.rootViewController = router.rootViewController
-
-
-                    router.showMainTabBarScreen()
-
-
-                    let tabBarController = router.rootViewController?.presentedViewController as! UITabBarController
-
                     tabBarController.selectTab(withTitle: "グラフ")
+
+
                     expect(tabBarController.displayedTab(isAKindOf: GraphViewController.self)).to(beTrue())
+                }
+
+                it("リスト画面を再表示する") {
+                    tabBarController.selectTab(withTitle: "グラフ")
+                    tabBarController.selectTab(withTitle: "リスト")
+
+
+                    expect(tabBarController.displayedTab(isAKindOf: ListViewController.self)).to(beTrue())
                 }
                 
             }
