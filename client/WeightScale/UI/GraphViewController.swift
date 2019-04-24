b719abc17a8c6462ab5eaaceb0e32ef62c7f66cc
hyai <hyai0323@gmail.com>
2019-04-25 07:46:22 +0900

    グラフ画面のタイトルが見える

--- client/WeightScale/UI/GraphViewController.swift
+++ client/WeightScale/UI/GraphViewController.swift
@@ -1,5 +1,14 @@
 import UIKit
 
 class GraphViewController: UIViewController {
-    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        title = "グラフ"
+        view.backgroundColor = .white
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
