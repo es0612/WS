115ce7fe706b2ddcab545cc8cd208e680df71740
hyai <hyai0323@gmail.com>
2019-04-25 07:15:29 +0900

    設定画面のタイトルを設定

--- client/WeightScale/UI/SettingViewController.swift
+++ client/WeightScale/UI/SettingViewController.swift
@@ -1,5 +1,14 @@
 import UIKit
 
 class SettingViewController: UIViewController{
-    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        title = "設定"
+        view.backgroundColor = .white
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
