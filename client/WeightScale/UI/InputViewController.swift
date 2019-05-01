c6cc1d67b023be6d4286f74fa13b12be9ea75f8f
hyai <hyai0323@gmail.com>
2019-05-01 19:56:39 +0900

    implements load data flow

--- client/WeightScale/UI/InputViewController.swift
+++ client/WeightScale/UI/InputViewController.swift
@@ -3,7 +3,7 @@ import PureLayout
 
 protocol WeightRepository {
     func saveData(weight: Double)
-    func loadData()
+    func loadData() -> [WeightData]
 }
 
 class InputViewController: UIViewController {
