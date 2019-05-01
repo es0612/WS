18d27f2d26f09e04564f15223449a66578e81b4a
hyai <hyai0323@gmail.com>
2019-05-01 16:21:52 +0900

    implement save data flow

---
+++ client/WeightScale/Weight/LocalRealmWrapper.swift
@@ -0,0 +1,12 @@
+import RealmSwift
+
+class LocalRealmWrapper: RealmWrapper {
+    func saveData(weightData: WeightData) {
+        let realmDB = try! Realm()
+
+        try! realmDB.write{
+            realmDB.add(weightData, update: true)
+        }
+    }
+
+}
