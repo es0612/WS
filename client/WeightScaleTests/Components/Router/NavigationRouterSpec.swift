d07ba5bec86cd394ab00026fc3a9fa7a4912d5bd
hyai <hyai0323@gmail.com>
2019-05-02 10:36:44 +0900

    コードの整理

--- client/WeightScaleTests/Components/Router/NavigationRouterSpec.swift
+++ client/WeightScaleTests/Components/Router/NavigationRouterSpec.swift
@@ -1,13 +1,11 @@
 import Nimble
 import Quick
-import Succinct
 
 @testable import WeightScale
 
 class NavigationRouterSpec: QuickSpec {
     override func spec() {
         describe("Navigation router に関するテスト") {
-
             it("Input画面を初期表示する"){
                 let router = NavigationRouter(animated: false)
 
@@ -15,11 +13,11 @@ class NavigationRouterSpec: QuickSpec {
                 router.showInputScreen()
 
 
-                expect(router.rootViewController).to(beAKindOf(InputViewController.self))
+                expect(router.rootViewController)
+                    .to(beAKindOf(InputViewController.self))
             }
 
             describe("tab barについてのテスト") {
-
                 var tabBarController: UITabBarController!
 
                 beforeEach {
@@ -30,9 +28,7 @@ class NavigationRouterSpec: QuickSpec {
                     window.makeKeyAndVisible()
                     window.rootViewController = router.rootViewController
 
-
                     router.showMainTabBarScreen()
-
 
                     tabBarController =
                         router.rootViewController?.presentedViewController
@@ -40,21 +36,24 @@ class NavigationRouterSpec: QuickSpec {
                 }
 
                 it("リスト画面を初期表示する") {
-                    expect(tabBarController.displayedTab(isAKindOf: ListViewController.self)).to(beTrue())
+                    expect(tabBarController.displayedTab(isAKindOf: ListViewController.self))
+                        .to(beTrue())
                 }
 
                 it("設定画面を表示する") {
                     tabBarController.selectTab(withTitle: "設定")
 
 
-                    expect(tabBarController.displayedTab(isAKindOf: SettingViewController.self)).to(beTrue())
+                    expect(tabBarController.displayedTab(isAKindOf: SettingViewController.self))
+                        .to(beTrue())
                 }
 
                 it("グラフ画面を表示する") {
                     tabBarController.selectTab(withTitle: "グラフ")
 
 
-                    expect(tabBarController.displayedTab(isAKindOf: GraphViewController.self)).to(beTrue())
+                    expect(tabBarController.displayedTab(isAKindOf: GraphViewController.self))
+                        .to(beTrue())
                 }
 
                 it("リスト画面を再表示する") {
@@ -62,11 +61,10 @@ class NavigationRouterSpec: QuickSpec {
                     tabBarController.selectTab(withTitle: "リスト")
 
 
-                    expect(tabBarController.displayedTab(isAKindOf: ListViewController.self)).to(beTrue())
+                    expect(tabBarController.displayedTab(isAKindOf: ListViewController.self))
+                        .to(beTrue())
                 }
-                
             }
-
         }
     }
 }
