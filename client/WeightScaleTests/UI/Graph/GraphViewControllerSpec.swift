caef3fe5e41cee7461172de6eb20121e12135cb7
hyai <hyai0323@gmail.com>
2019-05-04 10:07:13 +0900

    データを読み込む処理をgraph VCに追加

--- client/WeightScaleTests/UI/Graph/GraphViewControllerSpec.swift
+++ client/WeightScaleTests/UI/Graph/GraphViewControllerSpec.swift
@@ -9,20 +9,65 @@ import Charts
 class GraphViewControllerSpec: QuickSpec {
     override func spec() {
         describe("graph view controller に関するテスト") {
+            var stubWeightRepository: StubWeightRepository!
             var graphViewController: GraphViewController!
 
             beforeEach {
-                graphViewController = GraphViewController()
+                stubWeightRepository = StubWeightRepository()
             }
 
-            it("タイトルが見える") {
-                expect(graphViewController.title)
-                    .to(equal("グラフ"))
+            describe("体重データがあるとき") {
+                beforeEach {
+                    let expectedWeightData = WeightData()
+                    expectedWeightData.dateString = "2019/5/1"
+                    expectedWeightData.weight = 50.0
+
+                    stubWeightRepository.loadData_returnValue =
+                        [expectedWeightData]
+
+                    graphViewController = GraphViewController(weightRepository: stubWeightRepository)
+                }
+
+                it("タイトルが見える") {
+                    expect(graphViewController.title)
+                        .to(equal("グラフ"))
+                }
+
+                it("グラフが見える") {
+                    expect(graphViewController.hasLineChartView())
+                        .to(beTrue())
+                }
+
+                it("体重データを読み込んで表示する") {
+                    expect(stubWeightRepository.loadData_wasCalled)
+                        .to(beTrue())
+
+
+                    let chartView
+                        = graphViewController.view.findLineChartView()
+                    expect(chartView?.lineData?.entryCount)
+                        .to(equal(1))
+                }
             }
 
-            it("グラフが見える") {
-                expect(graphViewController.hasLineChartView())
-                    .to(beTrue())
+            describe("体重データがないとき") {
+                beforeEach {
+                    stubWeightRepository.loadData_returnValue = []
+
+                    graphViewController = GraphViewController(weightRepository: stubWeightRepository)
+                }
+                
+                it("体重データを読み込む") {
+                    expect(stubWeightRepository.loadData_wasCalled)
+                        .to(beTrue())
+                }
+
+                it("データがない旨を表示") {
+                    let chartView
+                        = graphViewController.view.findLineChartView()
+                    expect(chartView?.lineData?.entryCount)
+                        .to(equal(0))
+                }
             }
         }
     }
