55e974d5e7c9dc6b00e39878cb3273d9a706aede
hyai <hyai0323@gmail.com>
2019-05-04 10:07:13 +0900

    データを読み込む処理をgraph VCに追加

--- client/WeightScale/UI/GraphViewController.swift
+++ client/WeightScale/UI/GraphViewController.swift
@@ -2,13 +2,17 @@ import UIKit
 import Charts
 
 class GraphViewController: TemplateViewController {
+    // MARK: - Injected Dependencies
+    private var weightRepository: WeightRepository?
+
     // MARK: - Views
     var chartView: LineChartView
 
     // MARK: - Initialization
-    override init() {
-        chartView = LineChartView()
+    init(weightRepository: WeightRepository? = nil) {
+        self.weightRepository = weightRepository
 
+        chartView = LineChartView()
 
         super.init()
     }
@@ -17,6 +21,14 @@ class GraphViewController: TemplateViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Override Methods
+    override func viewDidLoad() {
+        if let weightDataList
+            = weightRepository?.loadData() {
+
+            drawChart(weightDataList: weightDataList)
+        }
+    }
 
     override func configureConstraints() {
         chartView.autoPinEdgesToSuperviewEdges()
@@ -30,13 +42,30 @@ class GraphViewController: TemplateViewController {
         title = "グラフ"
         view.backgroundColor = .white
 
-        chartView.noDataText = "体重データがありません"
-        updateViewConstraints()
+        chartViewConfiguration()
 
     }
-
 }
 
 // MARK: - Private methods
 fileprivate extension GraphViewController {
+    func chartViewConfiguration() {
+        chartView.noDataText = "体重データがありません"
+    }
+
+    func drawChart(weightDataList: [WeightData]) {
+        var dataForChart: [Double] = []
+        weightDataList.forEach { weightData in
+            dataForChart.append(weightData.weight)
+        }
+
+        var entries = [ChartDataEntry]()
+        for (i, d) in dataForChart.enumerated() {
+            entries.append(ChartDataEntry(x: Double(i), y: d))
+        }
+
+        let dataSet = LineChartDataSet(entries: entries, label: "my weight")
+
+        chartView.data = LineChartData(dataSet: dataSet)
+    }
 }
