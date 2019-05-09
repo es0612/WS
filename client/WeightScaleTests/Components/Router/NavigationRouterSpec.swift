9cfdca2820dd3de60546058d7feb9070f18f48c1
hyai <hyai0323@gmail.com>
2019-05-10 01:42:21 +0900

    アプリ起動時の初期画面をList VCに変更

--- client/WeightScaleTests/Components/Router/NavigationRouterSpec.swift
+++ client/WeightScaleTests/Components/Router/NavigationRouterSpec.swift
@@ -6,39 +6,21 @@ import Quick
 class NavigationRouterSpec: QuickSpec {
     override func spec() {
         describe("Navigation router に関するテスト") {
-            it("Input画面を初期表示する"){
-                let router = NavigationRouter(animated: false)
-
-
-                router.showInputScreen()
-
-
-                let navController = router.rootViewController as! UINavigationController
-                expect(navController.viewControllers.first)
-                    .to(beAKindOf(InputViewController.self))
-            }
-
             describe("tab barについてのテスト") {
                 var tabBarController: UITabBarController!
 
                 beforeEach {
                     let router = NavigationRouter(animated: false)
-                    router.rootViewController = UIViewController()
 
-                    let window = UIWindow(frame: UIScreen.main.bounds)
-                    window.makeKeyAndVisible()
-                    window.rootViewController = router.rootViewController
 
                     router.showMainTabBarScreen()
 
-                    tabBarController =
-                        router.rootViewController?.presentedViewController
-                        as? UITabBarController
+
+                    tabBarController = router.rootViewController as? UITabBarController
                 }
 
-                it("リスト画面を初期表示する") {
-                    expect(tabBarController.displayedTab(isAKindOf: ListViewController.self))
-                        .to(beTrue())
+                it("List画面を初期表示する"){
+                    expect(tabBarController?.displayedTab(isAKindOf: ListViewController.self)).to(beTrue())
                 }
 
                 it("設定画面を表示する") {
@@ -65,6 +47,24 @@ class NavigationRouterSpec: QuickSpec {
                     expect(tabBarController.displayedTab(isAKindOf: ListViewController.self))
                         .to(beTrue())
                 }
+            }
+
+            it("Input画面を表示する"){
+                let router = NavigationRouter(animated: false)
+                router.rootViewController = UIViewController()
+
+                let window = UIWindow(frame: UIScreen.main.bounds)
+                window.makeKeyAndVisible()
+                window.rootViewController = router.rootViewController
+
+
+                router.showInputScreen()
+
+
+                let navController = router.rootViewController?.presentedViewController as? UINavigationController
+
+                expect(navController?.viewControllers.first)
+                    .to(beAKindOf(InputViewController.self))
             }
         }
     }
