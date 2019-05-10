300a7cca111d9f4e37dc1162ab049794a8b9c6a9
hyai <hyai0323@gmail.com>
2019-05-10 19:58:37 +0900

    extract RunLoop method to a file

--- client/WeightScaleTests/Components/Router/NavigationRouterSpec.swift
+++ client/WeightScaleTests/Components/Router/NavigationRouterSpec.swift
@@ -89,14 +89,3 @@ class NavigationRouterSpec: QuickSpec {
         }
     }
 }
-
-private let oneHundredthOfASecond: TimeInterval = 0.5
-
-extension RunLoop {
-    static func advance(
-        by timeInterval: TimeInterval = oneHundredthOfASecond
-        ) {
-        let stopDate = Date().addingTimeInterval(timeInterval)
-        main.run(until: stopDate)
-    }
-}
