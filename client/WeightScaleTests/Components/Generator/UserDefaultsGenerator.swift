beae862399fa9f939b6ca33421d462f8afdc29c1
hyai <hyai0323@gmail.com>
2019-05-14 08:04:01 +0900

    add UserDefaults data generator

---
+++ client/WeightScaleTests/Components/Generator/UserDefaultsGenerator.swift
@@ -0,0 +1,33 @@
+import Nimble
+import Quick
+
+@testable import WeightScale
+
+class UserDefaultsGenerator: QuickSpec {
+    override func spec() {
+        xdescribe(
+            "UserDefaultsのテストデータを生成する(必要に応じてフォーカスして使う)"
+        ) {
+            let userDefaults = UserDefaults.standard
+
+            xit("目標体重を設定する") {
+                userDefaults.set(55.0, forKey: "target_weight")
+
+                let savedValue
+                    = userDefaults.double(forKey: "target_weight")
+
+                expect(savedValue).to(equal(55.0))
+            }
+
+            xit("目標体重を削除する") {
+                userDefaults.removeObject(forKey: "target_weight")
+
+                let savedValue
+                    = userDefaults.double(forKey: "target_weight")
+
+                expect(savedValue).to(equal(0.0))
+            }
+        }
+    }
+}
+
