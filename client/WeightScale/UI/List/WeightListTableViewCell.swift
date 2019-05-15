14e34f7c9606554ccad2a1caad9a3647a1654905
hyai <hyai0323@gmail.com>
2019-05-16 08:33:38 +0900

    apply style for selected cell background color on list

--- client/WeightScale/UI/List/WeightListTableViewCell.swift
+++ client/WeightScale/UI/List/WeightListTableViewCell.swift
@@ -5,9 +5,19 @@ class WeightListTableViewCell: UITableViewCell {
         super.init(style: .value1,
                    reuseIdentifier: reuseIdentifier
         )
+
+        applyStyles()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func applyStyles() {
+        self.selectionStyle = .default
+        let selectedView = UIView()
+        selectedView.backgroundColor
+            = UIColor.background.main
+        self.selectedBackgroundView =  selectedView
     }
 }
